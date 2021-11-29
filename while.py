0e96134f7d7181b21e890a1efce2915cbb2908d4
Touhidul Shawan <touhidulshawan@gmail.com>
2021-11-29 23:06:41 +0600

    add while loop

---
+++ while.py
@@ -0,0 +1,37 @@
+# suppose we want to print 1 to 10 number
+
+# typical way we can use like
+
+# print("1")
+# print("2")
+# print("3")
+# print("4")
+# print("5")
+# print("6")
+# print("7")
+# print("8")
+# print("9")
+# print("10")
+
+# but if we want to print 1000 or more number than we have to write that amount
+# print statement
+
+# But this is not sufficient so we can use loop here
+
+# Loop is way to repeat some task over and over
+
+flag = 1
+
+while flag <= 10:
+    print(flag)
+    flag += 1
+
+
+# This is how we can use while loop in python
+
+
+
+
+
+
+
