6f444e802b1ac8a046498e7a7a899a4290506d6c
Touhidul Shawan <touhidulshawan@gmail.com>
2021-12-01 14:40:08 +0600

    add for loop

---
+++ for.py
@@ -0,0 +1,14 @@
+# declare a list first
+
+fruit = ["apple", "mango", "orange"]
+
+# uses of for
+for f in fruit:
+    print(f)
+
+# set
+
+numbers = set([1, 2, 3, 4, 5, 6])
+
+for num in numbers:
+    print(num)
