fd3c544303e7b21a3ceb365f248a628a9d1595ad
Touhidul Shawan <touhidulshawan@gmail.com>
2021-12-04 00:48:38 +0600

    add function

---
+++ function.py
@@ -0,0 +1,23 @@
+# How to create a function
+
+# create a function
+
+def greeting(name):
+    print(f"Hi there! {name} whats up?")
+
+
+# this is how we create a function. In python we use def keyword to define a
+# function
+
+# if we run now without calling the function. nothing will happen
+# message does not get printed
+# How to call or use a function
+# To use a function we just need to call the function like
+greeting("Mike")
+greeting("John")
+greeting("Sara")
+# now we can resue the code as many as we want to do
+# after calling the function- function got executed and show the print message
+# How send data through a function
+# Now see hot send data as argument
+# This is how we use function
